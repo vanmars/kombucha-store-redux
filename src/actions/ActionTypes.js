f391a615a790f743c0c9aa8eba16d72f17c9d9dd
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-04 12:54:15 -0800

    Add action constants file

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,6 @@
+export const ADD_PRODUCT = 'ADD_PRODUCT';
+export const DELETE_PRODUCT = 'DELETE_PRODUCT';
+export const TOGGLE_FORM = 'TOGGLE_FORM';
+export const SELECT_PRODUCT = 'SELECT_PRODUCT';
+export const RESET_PRODUCT = 'RESET_PRODUCT';
+export const TOGGLE_EDITING = 'TOGGLE_EDITING';
