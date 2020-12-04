0734bf232c30158ebed7e7c35b9062a5dfbcad94
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-04 08:22:57 -0800

    Write and FAIL test for productListReducer with null action type

---
+++ src/reducers/product-list-reducer.js
@@ -0,0 +1,10 @@
+import { v4 } from 'uuid'
+
+export default (state=[
+  {name: "Gingerberry Goddess", brand: "Vanessa's Kombuchary", price: 3.25, flavor: "Gingerberry", quantity: 1, id: v4()},
+  {name: "Mystic Mango", brand: "Booch Boulevard, LLC", price: 3.99, flavor: "Mango", quantity: 10, id: v4()},
+  {name: "Symphonic Straberry", brand: "Scarlett's Secret SCOBY", price: 4.99, flavor: "Strawberry", quantity: 50, id: v4()},
+  {name: "Regal Raspberry", brand: "Paradise Kombucha Co.", price: 2.99, flavor: "Raspberry", quantity: 124, id: v4()}
+], action) => {
+  return
+}
