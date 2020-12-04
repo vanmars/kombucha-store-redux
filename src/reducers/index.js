cd769a41806da57f44e19036566e11321d5a4600
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-04 09:32:00 -0800

    PASS test for rootReducer with null action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,8 @@ import productListReducer from './product-list-reducer';
 import formVisibleReducer from './form-visible-reducer';
 
 const rootReducer = combineReducers({
-
+  masterProductList: productListReducer,
+  formVisible: formVisibleReducer
 });
 
 export default rootReducer;
