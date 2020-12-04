460d14c0caa6d1ebba02594e8ae60ea7388e76ff
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-04 09:39:33 -0800

    PASS test for rootReducer check that initial state matches productListReducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,10 +1,19 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import productListReducer from '../../reducers/product-list-reducer';
+// import formVisibleReducer from '../../reducers/form-visible-reducer';
 
 describe('rootReducer', () => {
+  let store = createStore(rootReducer);
+
   test('should return defaul state if no action type is recognized', () => {
     expect(rootReducer({}, {type:null})).toEqual({
     masterProductList: {},
     formVisible: false
     });
+  });
+
+  test('Check that inital state of productListReducer matches root reducer', () => {
+    expect(store.getState().masterProductList).toEqual(productListReducer(undefined, {type: null}));
   });
 })
