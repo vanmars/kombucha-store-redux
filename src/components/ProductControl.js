5bca5497fd83273da397873bc87f6a49bf79bff8
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-04 09:11:01 -0800

    Dispatch actions to store masterProductList in state and update code to handle new props

--- src/components/ProductControl.js
+++ src/components/ProductControl.js
@@ -5,17 +5,18 @@ import NewProductForm from './NewProductForm'
 import UpdateProductForm from './UpdateProductForm'
 import ProductDetail from './ProductDetail'
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-class ProductControl extends React.Component {
+class ProductControl extends Component {
   constructor(props){
     super(props);
     this.state = {
-      masterProductList: [
-        {name: "Gingerberry Goddess", brand: "Vanessa's Kombuchary", price: 3.25, flavor: "Gingerberry", quantity: 1, id: v4()},
-        {name: "Mystic Mango", brand: "Booch Boulevard, LLC", price: 3.99, flavor: "Mango", quantity: 10, id: v4()},
-        {name: "Symphonic Straberry", brand: "Scarlett's Secret SCOBY", price: 4.99, flavor: "Strawberry", quantity: 50, id: v4()},
-        {name: "Regal Raspberry", brand: "Paradise Kombucha Co.", price: 2.99, flavor: "Raspberry", quantity: 124, id: v4()}
-      ],
+      // masterProductList: [
+      //   {name: "Gingerberry Goddess", brand: "Vanessa's Kombuchary", price: 3.25, flavor: "Gingerberry", quantity: 1, id: v4()},
+      //   {name: "Mystic Mango", brand: "Booch Boulevard, LLC", price: 3.99, flavor: "Mango", quantity: 10, id: v4()},
+      //   {name: "Symphonic Straberry", brand: "Scarlett's Secret SCOBY", price: 4.99, flavor: "Strawberry", quantity: 50, id: v4()},
+      //   {name: "Regal Raspberry", brand: "Paradise Kombucha Co.", price: 2.99, flavor: "Raspberry", quantity: 124, id: v4()}
+      // ],
       formVisible: false,
       selectedProduct: null,
       editing: false
@@ -40,16 +41,27 @@ class ProductControl extends React.Component {
 
   // Create New Product
   handleCreatingProduct = (newProduct) => {
-    const newMasterProductList = this.state.masterProductList.concat(newProduct);
+    const { dispatch } = this.props;
+    const { name, brand, price, flavor, quantity, id} = newProduct;
+    const action  = {
+      type: 'ADD_PRODUCT',
+      name: name,
+      brand: brand,
+      price: price,
+      flavor: flavor,
+      quantity: quantity,
+      id: id
+    };
+    dispatch(action);
     this.setState({
-      masterProductList: newMasterProductList, 
+      // masterProductList: newMasterProductList, 
       formVisible: false
     });
   }
 
   // Read Individual Product Details
   handleSelectingProduct = (id) => {
-    const selectedProduct = this.state.masterProductList.filter(product => product.id === id)[0];
+    const selectedProduct = this.props.masterProductList[id];
     this.setState({selectedProduct: selectedProduct});
   }
 
@@ -59,9 +71,21 @@ class ProductControl extends React.Component {
   }
 
   handleUpdatingProduct = (productToUpdate) => {
-    const updatedMasterProductList = this.state.masterProductList.filter(product => product.id !== this.state.selectedProduct.id).concat(productToUpdate);
+    const { dispatch } = this.props;
+    const { name, brand, price, flavor, quantity, id} = productToUpdate;
+    const action  = {
+      type: 'ADD_PRODUCT',
+      name: name,
+      brand: brand,
+      price: price,
+      flavor: flavor,
+      quantity: quantity,
+      id: id
+    };
+    dispatch(action);
+    // const updatedMasterProductList = this.state.masterProductList.filter(product => product.id !== this.state.selectedProduct.id).concat(productToUpdate);
     this.setState({
-      masterProductList: updatedMasterProductList, 
+      // masterProductList: updatedMasterProductList, 
       editing: false, 
       selectedProduct: null
     });
@@ -69,27 +93,57 @@ class ProductControl extends React.Component {
 
   // Sell Product
   handleSellingProduct = (productToSell) => {
-    const updatedMasterProductList = this.state.masterProductList.filter(product => product.id !== productToSell.id).concat(productToSell);
+    const { dispatch } = this.props;
+    const { name, brand, price, flavor, quantity, id} = productToSell;
+    const action  = {
+      type: 'ADD_PRODUCT',
+      name: name,
+      brand: brand,
+      price: price,
+      flavor: flavor,
+      quantity: quantity,
+      id: id
+    };
+    dispatch(action);
+    // const updatedMasterProductList = this.state.masterProductList.filter(product => product.id !== productToSell.id).concat(productToSell);
     this.setState({
-      masterProductList: updatedMasterProductList,
-      // selectedProduct: null
+      // masterProductList: updatedMasterProductList,
+      selectedProduct: null
     })
   }
 
   // Restock Product
   handleRestockingProduct = (productToRestock) => {
-    const updatedMasterProductList = this.state.masterProductList.filter(product => product.id !== productToRestock.id).concat(productToRestock);
+    const { dispatch } = this.props;
+    const { name, brand, price, flavor, quantity, id} = productToRestock;
+    const action  = {
+      type: 'ADD_PRODUCT',
+      name: name,
+      brand: brand,
+      price: price,
+      flavor: flavor,
+      quantity: quantity,
+      id: id
+    };
+    dispatch(action);
+    // const updatedMasterProductList = this.state.masterProductList.filter(product => product.id !== productToRestock.id).concat(productToRestock);
     this.setState({
-      masterProductList: updatedMasterProductList,
+      // masterProductList: updatedMasterProductList,
       selectedProduct: null
     })
   }
 
   // Delete Individual Product
   handleDeletingProduct = (id) => {
-    const newMasterProductList = this.state.masterProductList.filter(product => product.id !== id);
+    const { dispatch } = this.props;
+    const action  = {
+      type: 'DELETE_PRODUCT',
+      id: id
+    };
+    dispatch(action);
+    // const newMasterProductList = this.state.masterProductList.filter(product => product.id !== id);
     this.setState({
-      masterProductList: newMasterProductList, 
+      // masterProductList: newMasterProductList, 
       selectedProduct: null
     });
   }
@@ -130,7 +184,7 @@ class ProductControl extends React.Component {
     } else {
       currentlyVisibleState = 
       <ProductList
-        products={this.state.masterProductList}
+        products={this.props.masterProductList}
         onProductSelection={this.handleSelectingProduct}
         onClickingSell={this.handleSellingProduct}
         onClickingRestock={this.handleRestockingProduct}
@@ -149,6 +203,16 @@ class ProductControl extends React.Component {
   }
 }
 
-ProductControl = connect()(ProductControl);
+const mapStateToProps = state => {
+  return {
+    masterProductList: state
+  };
+}
+
+ProductControl.propTypes = {
+  masterProductList: PropTypes.object
+}
+
+ProductControl = connect(mapStateToProps)(ProductControl);
 
 export default ProductControl;
