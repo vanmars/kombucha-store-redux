ffb5c38c71c857faa297586279078ff7152d4846
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-04 13:56:27 -0800

    Add four products to masterProductList default state and update tests accordingly

--- src/components/ProductControl.js
+++ src/components/ProductControl.js
@@ -7,12 +7,6 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import * as a from '../actions';
 
-// masterProductList: [
-      //   {name: "Gingerberry Goddess", brand: "Vanessa's Kombuchary", price: 3.25, flavor: "Gingerberry", quantity: 1, id: v4()},
-      //   {name: "Mystic Mango", brand: "Booch Boulevard, LLC", price: 3.99, flavor: "Mango", quantity: 10, id: v4()},
-      //   {name: "Symphonic Straberry", brand: "Scarlett's Secret SCOBY", price: 4.99, flavor: "Strawberry", quantity: 50, id: v4()},
-      //   {name: "Regal Raspberry", brand: "Paradise Kombucha Co.", price: 2.99, flavor: "Raspberry", quantity: 124, id: v4()}
-// ]
 class ProductControl extends Component {
   // constructor(props){
   //   super(props);
