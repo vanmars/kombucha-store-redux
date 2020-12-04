ee0e90d7974d5f25d3d9f63e48dc397f8c0068fa
Vanessa Stewart <vstewart1213@gmail.com>
2020-12-04 09:48:55 -0800

    Import rootReducer into app entry point

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { createStore } from 'redux';
-import reducer from './reducers/product-list-reducer';
+import rootReducer from './reducers/index';
 import { Provider } from 'react-redux';
 
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
